85eef62d1c73c84822c9f7e4dc19e42a3d94171d
brainchemist2005 <zackariaebouargan@gmail.com>
2023-08-25 01:26:17 -0400

    I did some modifications on the db file

--- database.py
+++ database.py
@@ -1,22 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('invoices.db')
-cursor = connection.cursor()
-
 #Invoice table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Invoices (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     user TEXT NOT NULL,
+    name TEXT NOT NULL,
     date DATE NOT NULL,
-    totalPrice REAL
+    totalPrice REAL,
+    isPaid INTEGER
 )
 ''')
 
 #Clothes table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Clothes (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     invoice_id INTEGER,
     clothes_detail TEXT NOT NULL,
     color TEXT,
@@ -26,5 +25,49 @@ CREATE TABLE IF NOT EXISTS Clothes (
 )
 ''')
 
-connection.commit()
-connection.close()
+class Database:
+    def __init__(self):
+        self.connection = None
+
+    def get_connection(self):
+        if self.connection is None:
+            self.connection = sqlite3.connect('db/invoices.db')
+        return self.connection
+
+    def disconnect(self):
+        if self.connection is not None:
+            self.connection.close()
+
+    def get_invoices(self):
+        cursor = self.get_connection().cursor()
+        query = ("select id,name,date,totalPrice,isPaid,clothes_detail,color,details from invoices, Clothes")
+        cursor.execute(query)
+        all_data = cursor.fetchall()
+        return [_build_invoice(item) for item in all_data]
+
+    def get_invoice(self, invoice_id):
+        cursor = self.get_connection().cursor()
+        query = ("select id,name,date,totalPrice,isPaid,details,color from invoices,clothes where id = ?")
+        cursor.execute(query, (invoice_id,))
+        item = cursor.fetchone()
+        if item is None:
+            return item
+        else:
+            return _build_invoice(item)
+
+    def add_invoice(user, name, date, totalPrice, isPaid, clothes_detail, color, price, details):
+        connection = self.get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("INSERT INTO Invoices (user, name, date, totalPrice, isPaid) VALUES (?, ?, ?, ?, ?)",
+                    (user, name, date, totalPrice, isPaid))
+
+        invoice_id = cursor.lastrowid
+
+        cursor.execute("INSERT INTO Clothes (invoice_id, clothes_detail, color, price, details) VALUES (?, ?, ?, ?, ?)",
+                    (invoice_id, clothes_detail, color, price, details))
+
+        connection.commit()
+        connection.close()
+
+        return invoice_id
